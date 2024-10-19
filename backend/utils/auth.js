870b3886f1f13741f14e63c58c95862c0e708ada
MarkSabangan <marksabangan1217@gmail.com>
2024-10-18 18:30:04 -0700

    added requireAuthorization

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,73 +1,94 @@
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const { User, Spot } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
 // Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-    // Create the token.
-    const safeUser = {
-      id: user.id,
-      email: user.email,
-      username: user.username,
-    };
-    const token = jwt.sign(
-      { data: safeUser },
-      secret,
-      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
-    );
-  
-    const isProduction = process.env.NODE_ENV === "production";
-  
-    // Set the token cookie
-    res.cookie('token', token, {
-      maxAge: expiresIn * 1000, // maxAge in milliseconds
-      httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction && "Lax"
-    });
-  
-    return token;
+  // Create the token.
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
   };
+  const token = jwt.sign(
+    { data: safeUser },
+    secret,
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+  );
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  // Set the token cookie
+  res.cookie('token', token, {
+    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && "Lax"
+  });
+
+  return token;
+};
 
 const restoreUser = (req, res, next) => {
-    // token parsed from cookies
-    const { token } = req.cookies;
-    req.user = null;
-  
-    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-      if (err) {
-        return next();
-      }
-  
-      try {
-        const { id } = jwtPayload.data;
-        req.user = await User.findByPk(id, {
-          attributes: {
-            include: ['email', 'createdAt', 'updatedAt']
-          }
-        });
-      } catch (e) {
-        res.clearCookie('token');
-        return next();
-      }
-  
-      if (!req.user) res.clearCookie('token');
-  
+  // token parsed from cookies
+  const { token } = req.cookies;
+  req.user = null;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
       return next();
-    });
-  };
+    }
 
-  // If there is no current user, return an error
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.findByPk(id, {
+        attributes: {
+          include: ['email', 'createdAt', 'updatedAt']
+        }
+      });
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+// If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
-    if (req.user) return next();
-  
-    const err = new Error('Authentication required');
-    err.title = 'Authentication required';
-    err.errors = { message: 'Authentication required' };
-    err.status = 401;
-    return next(err);
-  }
+  if (req.user) return next();
 
-module.exports = { setTokenCookie, restoreUser, requireAuth };
+  const err = new Error('Authentication required');
+  err.title = 'Authentication required';
+  err.errors = { message: 'Authentication required' };
+  err.status = 401;
+  return next(err);
+}
+const requireAuthorization = async (req, res, next) => {
+  const { spotId } = req.params;
+  const userId = req.user.id;
+  try {
+    const spot = await Spot.findByPk(spotId);
+    if (!spot) {
+      const err = new Error('Spot not found');
+      err.status = 404;
+      return next(err);
+    }
+    if (spot.ownerId !== userId) {
+      const err = new Error('Forbbiden');
+      err.title = 'Forbidden';
+      err.errors = { message: 'Forbidden' }
+      err.status = 403;
+      return next(err);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { setTokenCookie, restoreUser, requireAuth, requireAuthorization };
