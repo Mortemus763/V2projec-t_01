596cad3282d4d885b7288ec76e5a7dfb3b531486
ilazarkov <ilazarkov@gmail.com>
2024-10-25 22:53:30 -0500

    fixed delete review image

--- backend/routes/api/reviewimages.js
+++ backend/routes/api/reviewimages.js
@@ -5,36 +5,35 @@ const router = express.Router();
 
 //Delete a Review Image
 router.delete("/:imageId", requireAuth, async (req, res) => {
-  const {id} = req.user;
-  const {imageId} = req.params;
+    const { id } = req.user;
+    const { imageId } = req.params;
 
-  const imageWithReview = await ReviewImage.findOne({
-      where: {id: imageId},
-      include: {model: Review}
-  });
+    const imageWithReview = await ReviewImage.findOne({
+        where: { id: imageId },
+        include: { model: Review }
+    });
 
-  const imageToDelete = await ReviewImage.findByPk(imageId);
+    const imageToDelete = await ReviewImage.findByPk(imageId);
+    if (imageToDelete) {
+        if (id === imageWithReview.Review.userId) {
+            await imageToDelete.destroy();
+            res.json({
+                "message": "Successfully deleted"
+            });
 
-  if (imageToDelete){
-      if (id === imageWithReview.Reviews.userId){
-          await imageToDelete.destroy();
-          res.json({
-              "message": "Successfully deleted"
-          });
+        } else {
+            res.status(403);
+            res.json({
+                "message": "Forbidden"
+            });
+        }
 
-      } else {
-          res.status(403);
-          res.json({
-              "message": "Forbidden"
-          });
-      }
-
-  } else {
-      res.status(404);
-      res.json({
-          "message": "Review Image couldn't be found"
-      });
-  }
+    } else {
+        res.status(404);
+        res.json({
+            "message": "Review Image couldn't be found"
+        });
+    }
 
 });
 module.exports = router
