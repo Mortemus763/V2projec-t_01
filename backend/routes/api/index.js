49c31b5e28194d3417d665221fd7afa9c64f292a
leighatropp <leighatropp@gmail.com>
2025-03-24 22:36:09 -0700

    changed routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,33 +1,38 @@
 
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
-const spotImagesRouter = require('./spotImage.js');
-const reviewRouter = require('./review.js');
-const reviewImagesRouter = require('./reviewimages.js');
-const { restoreUser } = require("../../utils/auth.js");
+//const router = require('express').Router();
+//const sessionRouter = require('./session.js');
+//const usersRouter = require('./users.js');
+//const spotsRouter = require('./spots.js');
+//const spotImagesRouter = require('./spotImage.js');
+//const reviewRouter = require('./review.js');
+//const reviewImagesRouter = require('./reviewimages.js');
+//const { restoreUser } = require("../../utils/auth.js");
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('/api');
 
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
-router.use(restoreUser);
+//router.use(restoreUser);
 
-router.use('/session', sessionRouter);
+//router.use('/session', sessionRouter);
 
-router.use('/users', usersRouter);
+//router.use('/users', usersRouter);
 
-router.use('/spots', spotsRouter);
+//router.use('/spots', spotsRouter);
 
-router.use('/reviews', reviewRouter);
+//router.use('/reviews', reviewRouter);
 
-router.use('/spot-images', spotImagesRouter);
+//router.use('/spot-images', spotImagesRouter);
 
-router.use('/review-images', reviewImagesRouter);
+//router.use('/review-images', reviewImagesRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+router.use('/api', apiRouter);
+
+//router.post('/test', (req, res) => {
+  //res.json({ requestBody: req.body });
+//});
 
 module.exports = router;
